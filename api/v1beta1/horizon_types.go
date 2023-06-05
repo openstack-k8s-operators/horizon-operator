53bba658fa9f7c5572744baefca79f862a96ffd1
Takashi Kajinami <tkajinam@redhat.com>
2023-06-05 09:05:30 +0900

    Display status in CLI

--- api/v1beta1/horizon_types.go
+++ api/v1beta1/horizon_types.go
@@ -141,6 +141,8 @@ type HorizonStatus struct {
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
+//+kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.conditions[0].status",description="Status"
+//+kubebuilder:printcolumn:name="Message",type="string",JSONPath=".status.conditions[0].message",description="Message"
 
 // Horizon is the Schema for the horizons API
 type Horizon struct {
